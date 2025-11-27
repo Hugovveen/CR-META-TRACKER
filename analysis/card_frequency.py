1c15bbb58aecedd53aa7be7cf776b08eff656581
Hugovveen <hugovanveen4@gmail.com>
2025-11-27 11:57:31 +0100

    fixed fetch and added iterative data collection, still working on the api keys and levels and more stuff just checking if all is added

--- analysis/card_frequency.py
+++ analysis/card_frequency.py
@@ -1,11 +1,16 @@
 import json
 from collections import Counter
 import plotly.express as px
+import pandas as pd
+
 
 with open("db/opponent_decks.json", "r") as f:
     data = json.load(f)
 
 counter = Counter()
+
+total_decks = len(data)
+
 
 for battle_time, deck in data.items():
     for card_name, card_info in deck.items():
@@ -16,9 +21,35 @@ top10 = counter.most_common(10)
 
 rows = []
 for card, count in top10:
-    pct = (count / total_cards) * 100
-    rows.append({"card": card, "count": count, "raw_%":pct})
+    raw_pct = (count / total_cards) * 100
+    deck_pct = (count / total_decks) * 100
+    rows.append({"card": card, "count": count, "raw_%":raw_pct, "deck_%":deck_pct})
 
-print(rows)
-#print(top10)
-#print(total_cards)
+df = pd.DataFrame(rows)
+df = df.sort_values("deck_%", ascending=False)
+
+fig = px.bar(
+    df,
+    x="card",
+    y="deck_%",
+    hover_data=["count", "raw_%", "deck_%"],
+    title="Top 10 meest voorkomende kaarten (deck-playrate)",
+    labels={"card": "Kaart", "deck_%": "Deck playrate (%)"}
+)
+
+# Optioneel: percentages als label boven de bars
+fig.update_traces(
+    text=df["deck_%"].round(1).astype(str) + "%",
+    textposition="outside"
+)
+
+fig.update_layout(
+    xaxis_tickangle=-45,
+    title_x=0.5,
+    plot_bgcolor="#0a0f2c",
+    paper_bgcolor="#0a0f2c",
+    font=dict(color="white"),
+    margin=dict(l=40, r=40, t=80, b=120)
+)
+
+fig.write_html("plot_3_top10.html")
